b0139ba39fbeac5a64112d99ce767c37f544f2bd
Ridham <rj443@njit.edu>
2026-02-23 12:18:41 -0500

    Add DB listener for register/login and session key storage via RabbitMQ

---
+++ database/listener/db.php
@@ -0,0 +1,13 @@
+<?php
+
+function getDb() {
+	$host = "127.0.0.1";
+	$db   = "ticketdb";
+	$user = "root";
+	$pass = "";
+
+	$dsn = "mysql:host=$host;$dbname=$db;charset=utf8mb4";
+	$pdo = new PDO($dsn, $user, $pass);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $pdo;
+}	
