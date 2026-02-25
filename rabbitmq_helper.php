cadc648451988ccf77bbcf9de770f99e531156ad
Ridham <rj443@njit.edu>
2026-02-25 13:11:10 -0500

    Syntax error fixed

--- rabbitmq_helper.php
+++ rabbitmq_helper.php
@@ -1,8 +1,9 @@
 <?php
+
 require_once(__DIR__ . "/rabbitmqphp_example/rabbitMQLib.inc");
 
-function sendToDB($msg) {
+function sendToDB($reg) {
 	$iniPath = __DIR__ . "/rabbitmqphp.example/testRabbitMQ.ini";
 	$client = new rabbitMQClient($iniPath, "testServer");
-	return $client->send_request($msg);
+	return $client->send_request($req);
 }
