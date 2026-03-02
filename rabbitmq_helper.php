16f21c0f320584e12fdd8c5759ff3f436077fabe
ae459 <ae459@njit.edu>
2026-03-02 12:56:56 -0500

    Update rabbitmq_helper.php

--- rabbitmq_helper.php
+++ rabbitmq_helper.php
@@ -3,7 +3,7 @@
 require_once(__DIR__ . "/rabbitmqphp_example/rabbitMQLib.inc");
 
 function sendToDB($reg) {
-	$iniPath = __DIR__ . "/rabbitmqphp.example/testRabbitMQ.ini";
+	$iniPath = __DIR__ . "/rabbitmqphp_example/testRabbitMQ.ini";
 	$client = new rabbitMQClient($iniPath, "testServer");
 	return $client->send_request($reg);
 }
