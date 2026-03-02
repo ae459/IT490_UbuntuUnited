f38d068598c1d5c5b6b785e195031b67da3e2de9
Ridham <rj443@njit.edu>
2026-03-02 11:10:00 -0500

    Finalize home page with session validation through RabbitMQ

--- home.php
+++ home.php
@@ -1,9 +1,28 @@
 <?php
 session_start();
+require_once(__DIR__ . "/rabbitmq_helper.php");
 
-if (!isset($_SESSION["user"])) {
+if (!isset($_SESSION["session_key"])) {
     header("Location: login.php");
     exit();
+}
+
+$req = array();
+$req["type"] = "validate_session";
+$req["request_id"] = uniqid();
+$req["session_key"] = $_SESSION["session_key"];
+
+$res = sendToDB($req);
+
+if(!isset($res["valid"]) || $res["valid"] != true) {
+	session_destroy();
+	header("Location: login.php");
+	exit();
+}
+
+$username = "";
+if (isset($res["username"])) {
+	$username = $res["username"];
 }
 ?>
 <!DOCTYPE html>
@@ -15,11 +34,11 @@ if (!isset($_SESSION["user"])) {
 
 <h2>Home Page</h2>
 
-<p>Welcome <?php echo $_SESSION["user"]; ?>!</p>
+<p>Welcome <?php echo $username;  ?>!</p>
 
-<form method="post" action="logout.php">
-    <button type="submit">Logout</button>
-</form>
+<p>This is the homepage after successful login.</p>
+
+<p><a href="logout.php">Logout</a></p>
 
 </body>
 </html>
