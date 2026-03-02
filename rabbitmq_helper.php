c2a131bc6ff75d2ba61b5ff2feb223be60d4a764
ae459 <ae459@njit.edu>
2026-03-02 12:23:25 -0500

    Restore rabbitmq_helper.php

--- rabbitmq_helper.php
+++ rabbitmq_helper.php
@@ -5,5 +5,5 @@ require_once(__DIR__ . "/rabbitmqphp_example/rabbitMQLib.inc");
 function sendToDB($reg) {
 	$iniPath = __DIR__ . "/rabbitmqphp.example/testRabbitMQ.ini";
 	$client = new rabbitMQClient($iniPath, "testServer");
-	return $client->send_request($req);
+	return $client->send_request($reg);
 }
