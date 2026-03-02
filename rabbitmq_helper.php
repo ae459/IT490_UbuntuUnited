122103a2a29350fe4f73d20cecdc23a59b1a0ba8
ae459 <ae459@njit.edu>
2026-03-02 12:14:47 -0500

    Update files: add css directory and remove rabbitmq_helper.php

--- rabbitmq_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once(__DIR__ . "/rabbitmqphp_example/rabbitMQLib.inc");
-
-function sendToDB($reg) {
-	$iniPath = __DIR__ . "/rabbitmqphp.example/testRabbitMQ.ini";
-	$client = new rabbitMQClient($iniPath, "testServer");
-	return $client->send_request($req);
-}
