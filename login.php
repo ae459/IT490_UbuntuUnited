ace208d33a4e2a78bda647e03c46832f00cbe00c
Ridham <rj443@njit.edu>
2026-03-04 13:07:55 -0500

    login.php updated

--- login.php
+++ login.php
@@ -4,32 +4,54 @@ require_once(__DIR__ . "/rabbitmq_helper.php");
 
 $error = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+ {
 
 	$username = $_POST["username"];
 	$password = $_POST["password"];
+echo "REGISTER\n";
+$req = [
+	"type" => "register",
+	"request_id" => uniqid(),
+	"username" => $username,
+	"password" => $password
 
-$req = array();
-    $req["type"] = "login";
-    $req["request_id"] = uniqid();
-    $req["username"] = $username;
-    $req["password"] = $password;
+];
+$res = $client->send_request($req);
+print_r($res);
 
-    $res = sendToDB($req);
+echo "\nBAD LOGIN\n";
+$req2 = [
+	"type" => "login",
+	"request_id" => uniqid(),
+	"username" => $username,
+	"password" => "wrongpass"
 
-    if (isset($res["success"]) && $res["success"] == true) {
-        $_SESSION["session_key"] = $res["session_key"];
-        header("Location: home.php");
-        exit();
-    } else {
-        if (is_array($res) && isset($res["message"])) {
-            $error = $res["message"];
-        } else {
-            $error = "Invalid username or password";
-        }
-    }
+];
+$res2 = $client->send_request($req2);
+print_r($res2);
 
-}   
+echo "\nGOOD LOGIN\n";
+$req3 = [
+	"type" => "login",
+	"request_id" => uniqid(),
+	"username" => $username,
+	"password" => $password
+];
+$res3 = $client->send_request($req3);
+print_r($res3);
+
+if (isset($res3["session_key"])) {
+	echo "\nVALIDATE SESSION\n";
+	$req4 = [
+		"type" => "validate_session",
+		"request_id" => uniqid(),
+		"session_key" => $res3["session_key"]
+	];
+	$res4 = $client->send_request($req4);
+	print_r($res4);
+}
+}
 ?>
 <!DOCTYPE html>
 <html>
